14175fd1151575cc2ef143e96b4f884875f445e4
Josh Weisman <josh.m.weisman@gmail.com>
2018-09-21 15:54:12 +0300

    Restore CONTENT headers and add variable for Expect
    Restore CONTENT headers- it seems that from some clients PHP doesn't have the duplicate headers. Consider adding an `array_unique` to clean header array
    
    Wrap handling of Expect header in config variable.

--- proxy.php
+++ proxy.php
@@ -30,6 +30,13 @@ define('CSAJAX_FILTERS', true);
 define('CSAJAX_FILTER_DOMAIN', false);
 
 /**
+ * Enables or disables Expect: 100-continue header. Some webservers don't 
+ * handle this header correctly.
+ * Recommended value: false
+ */
+define('CSAJAX_SUPPRESS_EXPECT', false);
+
+/**
  * Set debugging to true to receive additional messages - really helpful on development
  */
 define('CSAJAX_DEBUG', false);
@@ -57,7 +64,7 @@ $curl_options = array(
 // identify request headers
 $request_headers = array( );
 foreach ($_SERVER as $key => $value) {
-    if (strpos($key, 'HTTP_') === 0 ) {
+    if (strpos($key, 'HTTP_') === 0  ||  strpos($key, 'CONTENT_') === 0) {
         $headername = str_replace('_', ' ', str_replace('HTTP_', '', $key));
         $headername = str_replace(' ', '-', ucwords(strtolower($headername)));
         if (!in_array($headername, array( 'Host', 'X-Proxy-Url' ))) {
@@ -137,7 +144,12 @@ if ($request_method == 'GET' && count($request_params) > 0 && (!array_key_exists
 
 // let the request begin
 $ch = curl_init($request_url);
-array_push($request_headers, 'Expect:'); // Many hosts don't support 100-Expect
+
+// Suppress Expect header
+if (CSAJAX_SUPPRESS_EXPECT) {
+    array_push($request_headers, 'Expect:'); 
+}
+
 curl_setopt($ch, CURLOPT_HTTPHEADER, $request_headers);   // (re-)send headers
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);     // return response
 curl_setopt($ch, CURLOPT_HEADER, true);       // enabled response headers
